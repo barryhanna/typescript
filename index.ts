848eef4aec91a18e63eab60f2374f3388524bfa7
Barry Hanna <barryhanna@users.noreply.github.com>
2022-04-17 10:48:47 +0100

    Basic types

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log('Hello world');
+const isOpen: boolean = false;
+const userName: string = 'Baz';
+const myAge: number = 40;
+const list: number[] = [0, 1, 2, 4, 5];
+const me: [string, number, boolean] = ['Baz', 40, false];
+
+enum Job {
+  WebDev,
+  WebDesigner,
+  PM,
+}
+
+const job: Job = Job.WebDesigner;
+
+const phone: any = 'Pixel';
+const tablet: any = 3;
